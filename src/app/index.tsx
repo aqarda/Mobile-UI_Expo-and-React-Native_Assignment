e03cb5900dd196b1bc26b3be9474d5a18398f369
VrisAEP <fernandobanff@gmail.com>
2026-09-16 11:41:11 -0600

    Add top bar and profile

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      <View style={styles.topBar}>
+        <Text style={styles.logo}>facebook</Text>
+        <Text style={styles.topIcons}>➕ 🔍 💬</Text>
+      </View>
+
+      <View style={styles.authorRow}>
+        <View style={styles.avatar} />
+        <View>
+          <Text style={styles.name}>
+            Marvel Studios ✔️ · <Text style={styles.blue}>Follow</Text>
+          </Text>
+          <Text style={styles.gray}>2h · 🌐</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -11,7 +24,45 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "white",
+    paddingTop: 30,
+    paddingBottom: 10,
+    paddingHorizontal: 12,
+  },
+  topBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  logo: {
+    color: "#0866FF",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  topIcons: {
+    fontSize: 20,
+  },
+  authorRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 6,
+  },
+  avatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: "black",
+    marginRight: 10,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  blue: {
+    color: "#0866FF",
+    fontWeight: "normal",
+  },
+  gray: {
+    color: "gray",
   },
 });
